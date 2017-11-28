7f1f2d58a606f23ce251cbb57b050baf20a37f7a
Mark Petrovic <mpetrovic@paypal.com>
2017-11-28 08:48:18 -0800

    [master] cleanup main

--- main.go
+++ main.go
@@ -17,87 +17,98 @@ func main() {
 	trustAnchors := flag.String("roots", "", "File containing trust anchors in PEM format.  Defaults to system roots.")
 	flag.Parse()
 
-	conn, err := tls.Dial("tcp", *host, &tls.Config{
-		InsecureSkipVerify: true,
-	})
-	if err != nil {
-		log.Fatalf("failed to connect: %v\n", err.Error())
+	var cstate tls.ConnectionState
+
+	// Get connection state
+	{
+		conn, err := tls.Dial("tcp", *host, &tls.Config{
+			InsecureSkipVerify: true,
+		})
+		if err != nil {
+			log.Fatalf("failed to connect: %v\n", err.Error())
+		}
+
+		cstate = conn.ConnectionState()
+		conn.Close()
 	}
 
-	cstate := conn.ConnectionState()
-	conn.Close()
+	// Print negotiated TLS version and ciphersuite.
+	{
+		switch cstate.Version {
+		case tls.VersionTLS12:
+			fmt.Print("TLS1.2/")
+		case tls.VersionTLS11:
+			fmt.Print("TLS1.1/")
+		case tls.VersionTLS10:
+			fmt.Print("TLS1.0/")
+		case tls.VersionSSL30:
+			fmt.Print("SSL3.0/")
+		default:
+			log.Fatalf("unsupported TLS version: %d\n", cstate.Version)
+		}
 
-	switch cstate.Version {
-	case tls.VersionTLS12:
-		fmt.Print("TLS1.2/")
-	case tls.VersionTLS11:
-		fmt.Print("TLS1.1/")
-	case tls.VersionTLS10:
-		fmt.Print("TLS1.0/")
-	case tls.VersionSSL30:
-		fmt.Print("SSL3.0/")
-	default:
-		log.Fatalf("unsupported TLS version: %d\n", cstate.Version)
+		switch cstate.CipherSuite {
+		case tls.TLS_RSA_WITH_RC4_128_SHA:
+			fmt.Println("TLS_RSA_WITH_RC4_128_SHA")
+		case tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA:
+			fmt.Println("TLS_RSA_WITH_3DES_EDE_CBC_SHA")
+		case tls.TLS_RSA_WITH_AES_128_CBC_SHA:
+			fmt.Println("TLS_RSA_WITH_AES_128_CBC_SHA")
+		case tls.TLS_RSA_WITH_AES_256_CBC_SHA:
+			fmt.Println("TLS_RSA_WITH_AES_256_CBC_SHA")
+		case tls.TLS_RSA_WITH_AES_128_CBC_SHA256:
+			fmt.Println("TLS_RSA_WITH_AES_128_CBC_SHA256")
+		case tls.TLS_RSA_WITH_AES_128_GCM_SHA256:
+			fmt.Println("TLS_RSA_WITH_AES_128_GCM_SHA256")
+		case tls.TLS_RSA_WITH_AES_256_GCM_SHA384:
+			fmt.Println("TLS_RSA_WITH_AES_256_GCM_SHA384")
+		case tls.TLS_ECDHE_ECDSA_WITH_RC4_128_SHA:
+			fmt.Println("TLS_ECDHE_ECDSA_WITH_RC4_128_SHA")
+		case tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA:
+			fmt.Println("TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA")
+		case tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA:
+			fmt.Println("TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA")
+		case tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA:
+			fmt.Println("TLS_ECDHE_RSA_WITH_RC4_128_SHA")
+		case tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA:
+			fmt.Println("TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA")
+		case tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA:
+			fmt.Println("TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA")
+		case tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA:
+			fmt.Println("TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA")
+		case tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256:
+			fmt.Println("TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256")
+		case tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256:
+			fmt.Println("TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256")
+		case tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256:
+			fmt.Println("TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256")
+		case tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256:
+			fmt.Println("TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256")
+		case tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384:
+			fmt.Println("TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384")
+		case tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384:
+			fmt.Println("TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384")
+		case tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305:
+			fmt.Println("TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305")
+		case tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305:
+			fmt.Println("TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305")
+		default:
+			log.Fatalf("unknown ciphersuite: %d\n", cstate.CipherSuite)
+		}
 	}
 
-	switch cstate.CipherSuite {
-	case tls.TLS_RSA_WITH_RC4_128_SHA:
-		fmt.Println("TLS_RSA_WITH_RC4_128_SHA")
-	case tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA:
-		fmt.Println("TLS_RSA_WITH_3DES_EDE_CBC_SHA")
-	case tls.TLS_RSA_WITH_AES_128_CBC_SHA:
-		fmt.Println("TLS_RSA_WITH_AES_128_CBC_SHA")
-	case tls.TLS_RSA_WITH_AES_256_CBC_SHA:
-		fmt.Println("TLS_RSA_WITH_AES_256_CBC_SHA")
-	case tls.TLS_RSA_WITH_AES_128_CBC_SHA256:
-		fmt.Println("TLS_RSA_WITH_AES_128_CBC_SHA256")
-	case tls.TLS_RSA_WITH_AES_128_GCM_SHA256:
-		fmt.Println("TLS_RSA_WITH_AES_128_GCM_SHA256")
-	case tls.TLS_RSA_WITH_AES_256_GCM_SHA384:
-		fmt.Println("TLS_RSA_WITH_AES_256_GCM_SHA384")
-	case tls.TLS_ECDHE_ECDSA_WITH_RC4_128_SHA:
-		fmt.Println("TLS_ECDHE_ECDSA_WITH_RC4_128_SHA")
-	case tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA:
-		fmt.Println("TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA")
-	case tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA:
-		fmt.Println("TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA")
-	case tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA:
-		fmt.Println("TLS_ECDHE_RSA_WITH_RC4_128_SHA")
-	case tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA:
-		fmt.Println("TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA")
-	case tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA:
-		fmt.Println("TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA")
-	case tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA:
-		fmt.Println("TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA")
-	case tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256:
-		fmt.Println("TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA256")
-	case tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256:
-		fmt.Println("TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA256")
-	case tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256:
-		fmt.Println("TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256")
-	case tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256:
-		fmt.Println("TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256")
-	case tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384:
-		fmt.Println("TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384")
-	case tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384:
-		fmt.Println("TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384")
-	case tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305:
-		fmt.Println("TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305")
-	case tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305:
-		fmt.Println("TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305")
-	default:
-		log.Fatalf("unknown ciphersuite: %d\n", cstate.CipherSuite)
-	}
-
-	for k, v := range cstate.PeerCertificates {
-		fmt.Printf("\nCertificate[%d]\n", k)
-		fmt.Printf("Subject:\t%s\n", v.Subject.CommonName)
-		fmt.Printf("Issuer:\t\t%s\n", v.Issuer.CommonName)
-		fmt.Printf("Expires:\t%s\n", v.NotAfter.String())
-		fmt.Printf("DNS Names:\t%+v\n", v.DNSNames)
-		fmt.Printf("IP Addresses:\t%+v\n", v.IPAddresses)
-		fmt.Printf("SubjectKeyID:\t%s\n", strings.ToUpper(hex.EncodeToString(v.SubjectKeyId)))
-		fmt.Printf("AuthorityKeyID:\t%s\n", strings.ToUpper(hex.EncodeToString(v.AuthorityKeyId)))
+	// Print peer cert chain
+	{
+		for k, v := range cstate.PeerCertificates {
+			fmt.Printf("\nCertificate[%d]\n", k)
+			fmt.Printf("Subject:\t%s\n", v.Subject.CommonName)
+			fmt.Printf("Issuer:\t\t%s\n", v.Issuer.CommonName)
+			fmt.Printf("Expires:\t%s\n", v.NotAfter.String())
+			fmt.Printf("DNS Names:\t%+v\n", v.DNSNames)
+			fmt.Printf("IP Addresses:\t%+v\n", v.IPAddresses)
+			fmt.Printf("SubjectKeyID:\t%s\n", strings.ToUpper(hex.EncodeToString(v.SubjectKeyId)))
+			fmt.Printf("AuthorityKeyID:\t%s\n", strings.ToUpper(hex.EncodeToString(v.AuthorityKeyId)))
+		}
 	}
 
 	// Validate peer certificate chain
